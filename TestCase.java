ca68edd3bb60dbc93840ce8cdbd875716ebb164a
Andrew Thomas McNulty <atmcnulty@hotmail.com>
2020-09-06 14:03:33 -0400

    RedLine01
    failing test instantiating numbers class

---
+++ TestCase.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCase {
+
+	
+	
+	
+	@Test
+	public void test() {
+		Number n = new Number();
+		assertNotNull(n);
+	}
+
+}
